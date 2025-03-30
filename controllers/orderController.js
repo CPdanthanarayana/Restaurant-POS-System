d17aa23e55cd923154034af03f7ebf2737e2535a
chamath <dcpoorna887@gmail.com>
2025-03-30 21:50:35 +0530

    create order controllers and fix some erros in order router

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,9 +1,60 @@
-const addOrder = async (req, res, next) => {};
+const createHttpError = require("http-errors");
+const Order = require("../models/orderModel");
 
-const getOrder = async (req, res, next) => {};
+const addOrder = async (req, res, next) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res
+      .status(201)
+      .json({ success: true, message: "Order Created!", data: order });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getOrders = async (req, res, next) => {};
+const getOrderById = async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order) {
+      const error = createHttpError(404, "Order not found!");
+      return next(error);
+    }
 
-const updateOrder = async (req, res, next) => {};
+    res.status(200).json({ success: true, data: order });
+  } catch (error) {}
+};
 
-module.exports = { addOrder, getOrder, getOrders, updateOrder };
+const getOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json({ data: orders });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateOrder = async (req, res, next) => {
+  try {
+    const { orderStatus } = req.body;
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      { orderStatus },
+      { new: true }
+    );
+
+    if (!order) {
+      const error = createHttpError(404, "Order not found!");
+      return next(error);
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Order Updated!", data: order });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { addOrder, getOrderById, getOrders, updateOrder };
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,15 +2,15 @@ const express = require("express");
 const { isVerifiedUser } = require("../middlewares/tokenVerification");
 const {
   addOrder,
-  getOrder,
+  getOrderById,
   getOrders,
   updateOrder,
 } = require("../controllers/orderController");
 const router = express.Router();
 
 router.route("/").post(isVerifiedUser, addOrder);
-router.route("/").get(isVerifiedUser, getOrder);
 router.route("/").get(isVerifiedUser, getOrders);
-router.route("/").put(isVerifiedUser, updateOrder);
+router.route("/:id").get(isVerifiedUser, getOrderById);
+router.route("/:id").put(isVerifiedUser, updateOrder);
 
 module.exports = router;
